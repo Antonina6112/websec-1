270683107c91f5e97801e18311176e53ad7a189a
Tonya Forg <73228451+Antonina6112@users.noreply.github.com>
2022-10-26 16:59:11 +0400

    Add files via upload
    final version

--- script.js
+++ script.js
@@ -6,22 +6,30 @@ function buttonClickHandler()
     let value1 = document.getElementById("input1").value;
     let value2 = document.getElementById("input2").value;
 
-    var result;
-    switch (selectorElem.value){
-        case 'plus':
-            result = parseFloat(value1) + parseFloat(value2);
-            break;
-        case 'minus':
-            result = parseFloat(value1) - parseFloat(value2);
-            break;
-        case 'mult':
-            result = parseFloat(value1) * parseFloat(value2);
-            break;
-        case 'div':
-            result = parseFloat(value1) / parseFloat(value2);
-            break;
-    }
+    var result = "";
+	if(value1 === "" || value2 === ""){
+		alert("Поля не могут быть пустыми");
+    }else{
+		switch (selectorElem.value){
+			case 'plus':
+				result = parseFloat(value1) + parseFloat(value2);
+				break;
+			case 'minus':
+				result = parseFloat(value1) - parseFloat(value2);
+				break;
+			case 'mult':
+				result = parseFloat(value1) * parseFloat(value2);
+				break;
+			case 'div':
+				if(parseFloat(value2) === 0){
+                    result = "";
+                    alert("Делить на ноль нельзя!");
+                }else{
+					result = parseFloat(value1) / parseFloat(value2);
+				}
+				break;
+		}
+	}
 
     document.getElementById("results").innerText = result;
-    console.log(value1,value2)
 }
